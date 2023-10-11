e237fbd514ab2c235ffc42747b90ebd60f88d8a9
Sabir Mustafaev <114620144+SabirAga@users.noreply.github.com>
2023-10-11 02:55:09 +0200

    added logouthandler via useContext

--- src/App.js
+++ src/App.js
@@ -29,13 +29,15 @@ function App() {
   };
 
   return (
-      <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-        <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
-        </main>
-      </AuthContext.Provider>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
+    >
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </AuthContext.Provider>
   );
 }
 
